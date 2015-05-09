5d492fc51f8847c9bb2d16381ac6799863dc3a65
UnovaChampionN <hamzashahid2002@hotmail.com>
2015-05-09 19:27:48 +0500

    Update commands.js

--- commands.js
+++ commands.js
@@ -18,7 +18,7 @@ exports.commands = {
 	 * These commands are here to provide information about the bot.
 	 */
 	 
-	 credits: 'about',
+	credits: 'about',
 	about: function (arg, user, room) {
 		var text = (room === user || user.hasRank(room, '#')) ? '' : '/pm ' + user.id + ', ';
 		text += '**RKO-Bot:** A bot that hits an RKO outta nowhere...when you least expect it. Base repo made by TTT, modified by Ruby D.';
@@ -880,44 +880,22 @@ exports.commands = {
 };
 
 /* Pro Wrestling room commands */
-	smackdown: 'smackdown',
-	smackdownresults: function(arg, by, room, con) {
-		if (this.hasRank(by, '+%@#~') || room.charAt(0) === ',') {
-			var text = '';
-		} else {
-			var text = '/pm ' + by + ', ';
-		}
+	smackdown: 'smackdownresults',
+	smackdownresults: function (arg, user, room) {
+		var text = (room === user || user.hasRank(room, '+%@#')) ? '' : '/pm ' + user.id + ', ';
 		text += 'http://bleacherreport.com/articles/2449358-wwe-smackdown-results-winners-grades-reaction-and-highlights-from-april-30 **Smackdown Results: April 30th**';
-		this.say(con, room, text);
+		this.say(room, text);
 	},
 	
-	raw: 'rawresults',
-	rawresults: function(arg, by, room, con) {
-		if (this.hasRank(by, '+%@#~') || room.charAt(0) === ',') {
-			var text = '';
-		} else {
-			var text = '/pm ' + by + ', ';
-		}
+	rawresults: 'raw',
+	raw: function (arg, user, room) {
+		var text = (room === user || user.hasRank(room, '+%@#')) ? '' : '/pm ' + user.id + ', ';
 		text += 'http://bleacherreport.com/articles/2453752-wwe-raw-results-winners-grades-reaction-and-highlights-from-may-4 **Raw Results: May 4th**';
-		this.say(con, room, text);
+		this.say(room, text);
 	},
-	
-	ppv: function(arg, by, room, con) {
-		if (this.hasRank(by, '+%@#~') || room.charAt(0) === ',') {
-			var text = '';
-		} else {
-			var text = '/pm ' + by + ', ';
-		}
-		text += "This month's PPV is **Payback**.";
-		this.say(con, room, text);
-	},
-	
-	ppvresults: function(arg, by, room, con) {
-		if (this.hasRank(by, '+%@#~') || room.charAt(0) === ',') {
-			var text = '';
-		} else {
-			var text = '/pm ' + by + ', ';
-		}
-		text += "**Extreme Rules Results** http://bleacherreport.com/articles/2442937-wwe-extreme-rules-2015-results-biggest-winners-and-losers-from-ppv";
-		this.say(con, room, text);
+
+	ppv: function (arg, user, room) {
+		var text = (room === user || user.hasRank(room, '+%@#')) ? '' : '/pm ' + user.id + ', ';
+		text += "This month's PPV is **Payback** | Results for Extreme Rules: http://bleacherreport.com/articles/2442937-wwe-extreme-rules-2015-results-biggest-winners-and-losers-from-ppv ";
+		this.say(room, text);
 	},
